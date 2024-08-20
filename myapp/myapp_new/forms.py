e7289526352076c26483379e259d4c429de02dcc
Karthik3105 <83666897+Karthik3105@users.noreply.github.com>
2024-08-20 16:03:00 +0530

    Delete myapp directory

--- myapp/myapp_new/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Resume
-
-
-class ResumeForm(forms.ModelForm):
-    class Meta:
-        model=Resume
-        fields=['id','name','dob','gender','locality','city','pin','state',
-                  'mobile','job_city','profile_image','my_file']
